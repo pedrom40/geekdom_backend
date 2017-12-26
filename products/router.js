25c34957a263b624dc57bbfa5635348516bd99c2
pedrom40 <pedro.morin1980@gmail.com>
2017-12-26 11:45:06 -0600

    add cors to products router

--- products/router.js
+++ products/router.js
@@ -6,6 +6,17 @@ const jsonParser = bodyParser.json();
 // pull in db connection
 const db = require('../database');
 
+// CORS
+router.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 // GET: return all categories in use
 router.get('/categories', jsonParser, (req, res) => {
 
